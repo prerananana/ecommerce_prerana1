122a460f28691dcf949789148264ddb1e3eb8507
Prerana Mali <prerana@Preranas-MacBook-Air.local>
2022-06-04 13:29:48 +0545

    Second lab git commit

--- product_module/admin.py
+++ product_module/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Brand, Category
+from .models import Brand, Category, Product
 admin.site.register(Brand)
 admin.site.register(Category)
+admin.site.register(Product)
--- product_module/models.py
+++ product_module/models.py
@@ -10,3 +10,14 @@ class Category(models.Model):
     is_active = models.BooleanField()
 class Meta:
     verbose_name_plural = "Categories"
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    quantity = models.IntegerField()
+    image_url = models.CharField(max_length=500)
+    color_code = models.CharField(max_length=20)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    registered_on = models.DateTimeField()
+    is_active = models.BooleanField()
