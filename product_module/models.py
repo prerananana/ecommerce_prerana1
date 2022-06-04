3e0c2a16ba56fc4b4d0e140a78168d7a5a738c21
Prerana Mali <prerana@Preranas-MacBook-Air.local>
2022-06-04 13:44:29 +0545

    Third Lab completed

--- product_module/models.py
+++ product_module/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.html import mark_safe
 
 # Create your models here.
 class Brand(models.Model):
@@ -21,3 +22,8 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     registered_on = models.DateTimeField()
     is_active = models.BooleanField()
+    def image_tag(self):
+        return mark_safe(f'<img src="{self.image_url}" width="50" height="50" />')
+        image_tag.short_description = "Product"
+    def __str__(self):
+        return self.name
